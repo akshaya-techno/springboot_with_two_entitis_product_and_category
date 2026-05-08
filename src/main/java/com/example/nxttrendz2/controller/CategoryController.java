bbc99ae591a25a3af18dbd5ad99e80158a256c8f
Akshaya-techno <akshayatechnochan01@gamil.com>
2026-05-08 14:10:41 +0000

    before final commit 1

--- src/main/java/com/example/nxttrendz2/controller/CategoryController.java
+++ src/main/java/com/example/nxttrendz2/controller/CategoryController.java
@@ -9,3 +9,44 @@
  */
 
 // Write your code here
+package com.example.nxttrendz2.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import com.example.nxttrendz2.service.CategoryJpaService;
+import com.example.nxttrendz2.model.*;
+
+@RestController
+public class CategoryController {
+
+    @Autowired
+    private CategoryJpaService categoryJpaService;
+
+    @GetMapping("/categories")
+    public List<Category> getCategories() {
+        return categoryJpaService.getCategories();
+    }
+
+    @PostMapping("/categories")
+    public Category addCategory(@RequestBody Category category) {
+        return categoryJpaService.addCategory(category);
+    }
+
+    @GetMapping("/categories/{categoryId}")
+    public Category getCategoryById(@PathVariable int categoryId) {
+        return categoryJpaService.getCategoryById(categoryId);
+    }
+
+    @PutMapping("/categories/{categoryId}")
+    public Category updateCategory(@PathVariable int categoryId, @RequestBody Category category) {
+        return categoryJpaService.updateCategory(categoryId, category);
+    }
+
+    @DeleteMapping("/categories/{categoryId}")
+    public void deleteCategory(@PathVariable int categoryId) {
+        categoryJpaService.deleteCategory(categoryId);
+    }
+}
