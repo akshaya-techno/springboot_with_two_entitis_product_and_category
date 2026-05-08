e86d97f46d3ac5aafd4053d5a92651a2adaf781c
Akshaya-techno <akshayatechnochan01@gamil.com>
2026-05-08 13:19:17 +0000

    CategoryJpa service commit

--- src/main/java/com/example/nxttrendz2/service/CategoryJpaService.java
+++ src/main/java/com/example/nxttrendz2/service/CategoryJpaService.java
@@ -12,3 +12,64 @@
  */
 
 // Write your code here
+package com.example.nxttrendz2.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.*;
+
+import com.example.nxttrendz2.repository.CategoryJpaRepository;
+import com.example.nxttrendz2.repository.CategoryRepository;
+import com.example.nxttrendz2.model.*;
+
+@Service
+public class CategoryJpaService implements CategoryRepository {
+
+    @Autowired
+    private CategoryJpaRepository categoryJpaRepository;
+
+    @Override
+    public List<Category> getCategories() {
+        List<Category> categoryList = categoryJpaRepository.findAll();
+        return new ArrayList<>(categoryList);
+    }
+
+    @Override
+    public Category addCategory(Category category) {
+        return categoryJpaRepository.save(category);
+    }
+
+    @Override
+    public Category getCategoryById(int categoryId) {
+        return categoryJpaRepository.findById(categoryId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @Override
+    public Category updateCategory(int categoryId, Category category) {
+        Category existingCategory = getCategoryById(categoryId);
+
+        if (category.getCategoryName() != null) {
+            existingCategory.setCategoryName(category.getCategoryName());
+        }
+        if (category.getCategoryDescription() != null) {
+            existingCategory.setCategoryDescription(category.getCategoryDescription());
+        }
+
+        return categoryJpaRepository.save(existingCategory);
+    }
+
+    @Override
+    public void deleteCategory(int categoryId) {
+        try{
+            Category category = getCategoryById(categoryId);
+            categoryJpaRepository.delete(category);
+        }catch(Exception e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        
+    }
+}
