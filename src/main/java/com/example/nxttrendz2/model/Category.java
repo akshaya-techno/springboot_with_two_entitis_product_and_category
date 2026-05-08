259eb26c196733628a15f979cfe371d93cd696f1
Akshaya-techno <akshayatechnochan01@gamil.com>
2026-05-08 10:22:36 +0000

    category model layer commit

--- src/main/java/com/example/nxttrendz2/model/Category.java
+++ src/main/java/com/example/nxttrendz2/model/Category.java
@@ -7,3 +7,55 @@
  */
 
 // Write your code here
+package com.example.nxttrendz2.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    public Category() {
+    }
+
+    public Category(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public void setCategoryId(int id) {
+        this.id = id;
+    }
+
+    public int getCategoryId() {
+        return id;
+    }
+
+    public void setCategoryName(String name) {
+        this.name = name;
+    }
+
+    public String getCategoryName() {
+        return name;
+    }
+
+    public void setCategoryDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategoryDescription() {
+        return description;
+    }
+}
