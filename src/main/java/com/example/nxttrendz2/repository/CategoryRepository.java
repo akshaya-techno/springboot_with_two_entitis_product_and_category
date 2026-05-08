d4f6d1c3184fde15075430977849976a4c4842ed
Akshaya-techno <akshayatechnochan01@gamil.com>
2026-05-08 12:44:06 +0000

    categoryrepository model commit

--- src/main/java/com/example/nxttrendz2/repository/CategoryRepository.java
+++ src/main/java/com/example/nxttrendz2/repository/CategoryRepository.java
@@ -7,3 +7,20 @@
  */
 
 // Write your code here
+package com.example.nxttrendz2.repository;
+
+import com.example.nxttrendz2.model.Category;
+import java.util.List;
+
+public interface CategoryRepository {
+
+    List<Category> getCategories();
+
+    Category addCategory(Category category);
+
+    Category getCategoryById(int categoryId);
+
+    Category updateCategory(int categoryId, Category category);
+
+    void deleteCategory(int categoryId);
+}
