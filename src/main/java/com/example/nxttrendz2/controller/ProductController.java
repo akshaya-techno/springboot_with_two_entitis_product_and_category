3f26e34c59f0a5d77fea85fbfc5350a235774a80
Akshaya-techno <akshayatechnochan01@gmail.com>
2026-05-09 09:36:41 +0000

    final commit

--- src/main/java/com/example/nxttrendz2/controller/ProductController.java
+++ src/main/java/com/example/nxttrendz2/controller/ProductController.java
@@ -13,6 +13,7 @@ package com.example.nxttrendz2.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
 
 import java.util.List;
 
